16b636b8e588cc508447910baf2982bdf2fedecb
Kenneth Mead <kennethmead784@gmail.com>
2025-12-01 23:16:12 -0500

    added proper bundling for cjs with tsup

--- apps/cms-api/src/app.module.ts
+++ apps/cms-api/src/app.module.ts
@@ -1,19 +1,28 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
+// import { PassportModule } from '@nestjs/passport';
 // import { FirebaseModule } from '@haru-cafe/google';
 // import { FilesModule } from '@haru-cafe/files';
-import { DebugModule } from '@haru-cafe/debug';
-// import { AuthModule } from '@haru-cafe/auth';
 import { HttpModule } from './http.module';
+// import { DebugModule } from '@haru-cafe/debug';
+// import { AuthModule } from '@haru-cafe/auth';
+// import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { HaruCafeCmsModule } from '@haru-cafe/cms';
+// import { HaruCafeDrizzlePgModule } from '@haru-cafe/cms';
 
 @Module({
     imports: [
-        DebugModule,
+        // DevtoolsModule.register({
+        //     http: process.env.NODE_ENV !== 'production',
+        // }),
+        // DebugModule,
         HttpModule,
+        // HaruCafeDrizzlePgModule,
+        HaruCafeCmsModule,
         // FilesModule,
         // FirebaseModule,
         // AuthModule,
-        PassportModule.register({ defaultStrategy: 'google' }),
+        // PassportModule.register({ defaultStrategy: 'google' }),
     ],
+    // providers: [...HaruCafeDrizzlePgProviders, ...HaruCafeCmsProviders],
 })
 export class AppModule {}
--- apps/cms-api/src/main.ts
+++ apps/cms-api/src/main.ts
@@ -1,14 +1,22 @@
 import cookieParser from 'cookie-parser';
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, PartialGraphHost } from '@nestjs/core';
 import { HttpConfig } from './http.config';
 import { AppModule } from './app.module';
+import fs from 'fs';
+// import { DebugService } from '@haru-cafe/debug';
 
 const DEFAULT_PORT = 3000;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
+        snapshot: true,
+        abortOnError: false,
         logger: ['error', 'warn', 'log', 'debug'],
     });
+
+    // app.get(DebugService).listRoutes(app).forEach((route) => {
+    //     console.log(`Route: ${route}`);
+    // });
 
     app.enableCors({
         origin: 'http://localhost:4200',
@@ -22,4 +30,7 @@ async function bootstrap() {
     const httpConfig = app.get(HttpConfig);
     await app.listen(httpConfig.port || DEFAULT_PORT);
 }
-bootstrap();
+bootstrap().catch(() => {
+    fs.writeFileSync('graph.json', PartialGraphHost.toString() ?? '');
+    process.exit(1);
+});
